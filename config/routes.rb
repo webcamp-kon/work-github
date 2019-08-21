c60aa10044f191d773c7489822b67ea8bde4775c
NorHum1024 <devgobouman@gmail.com>
2019-08-22 02:23:54 +0900

    モデル「products」の作成、ビューページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for users
+  get 'products/search' => 'products#search'
+  get 'products/ranking' => 'products#ranking'
+  resources :products, only: [:index,:show]
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
