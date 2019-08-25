60db9c2b45065db0be4913e7b050ac9486e3c404
NorHum1024 <devgobouman@gmail.com>
2019-08-25 21:34:11 +0900

    completed model and relations

--- app/models/arrivalmanagement.rb
+++ app/models/arrivalmanagement.rb
@@ -1,2 +1,3 @@
 class Arrivalmanagement < ApplicationRecord
+    belongs_to :product
 end
--- test/models/addresse_test.rb
+++ test/models/addresse_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class AddresseTest < ActiveSupport::TestCase
+  belongs_to :user
   # test "the truth" do
   #   assert true
   # end
