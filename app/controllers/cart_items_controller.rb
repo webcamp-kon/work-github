6c0726199cdd56647e049e644c3d19ba9a0de6da
unknown <you@example.com>
2019-09-09 06:57:42 +0900

    9/9

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,22 +5,24 @@ class CartItemsController < ApplicationController
 		# @sum =CartItem.where(user_id: current_user.id).products.sum(:price)
 	end
 	def plus
-		@cart_item = CartItem.find(params[:id])
-		@cart_item.order_number += 1
-		@cart_item.update
+		cart_item = CartItem.find(params[:cart_item_id])
+		cart_item.increment(:order_number, 1)
+		cart_item.save
 		redirect_to cart_items_path
 	end
 	def minus
-		@cart_item = CartItem.find(params[:id])
-		@cart_item.order_number -= 1
-		@cart_item.update
+		cart_item = CartItem.find(params[:cart_item_id])
+		cart_item.decrement(:order_number, 1)
+		cart_item.save
 		redirect_to cart_items_path
 	end
 	def create
+		cart_items = current_user.cart_items
 		product = Product.find(params[:product_id])
+		if product
 		cart_item = current_user.cart_items.new
 		cart_item.product_id = product.id
 		cart_item.save
-		redirect_to product_cart_items_path
+		redirect_to cart_items_path
 	end
 end
