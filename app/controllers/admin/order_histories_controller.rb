9df404a3ae067e23ee34f3468b4920e0c69c8530
akirakomiya <komiya.akira.infratop@gmail.com>
2019-09-26 08:13:37 +0000

    進捗経過

--- app/controllers/admin/order_histories_controller.rb
+++ app/controllers/admin/order_histories_controller.rb
@@ -4,12 +4,13 @@ class Admin::OrderHistoriesController < ApplicationController
     @order_histories=OrderHistory.page(params[:page]).per(10)
   end
   def edit
-    @order_histories=OrderHistory.find(params[:id])
-    @artists=Artist.all
-    @labels=Label.all
-    @genres=Genre.all
+    @order_history=OrderHistory.find(params[:id])
+    @order_list = @order_history.order_list
   end
-   def update
+  def show
+    @order_history=OrderHistory.find(params[:id])
+  end
+  def update
     @order_histories=OrderHistory.find(params[:id])
     if @order_histories.update(order_history_params)
        flash[:notice] ="You have updated order_history successfully."
@@ -22,5 +23,14 @@ class Admin::OrderHistoriesController < ApplicationController
     @order_histories=OrderHistory.find(params[:id])
     @order_histories.destroy
     redirect_to admin_order_histories_path
+  end
+  def order_history_params
+   params.require(:order_history).permit()
+  end
+  def order_list_params
+    params.require(:order_list).permit(:order_history_id,:product_id,:amount,:price)
+  end
+  def product_params
+     params.require(:product).permit(:jacket_image)
   end
 end
