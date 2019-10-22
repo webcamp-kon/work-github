dc19e15f02fea871b027e68c1403eb7e50977e85
unknown <you@example.com>
2019-10-22 18:22:19 +0900

    10

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,8 +16,11 @@ class Admin::ProductsController < ApplicationController
   end
   def create
     product=Product.new(product_params)
-    product.save
-    redirect_to admin_products_path
+    if product.save
+      redirect_to admin_products_path
+    else
+      redirect_to new_admin_product_path
+    end
   end
   def edit
     @product=Product.find(params[:id])
